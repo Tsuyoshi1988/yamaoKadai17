5b434cb17897a30a425e484e9c612565a8845642
akio0911 <akio0911@gmail.com>
2024-08-25 21:37:27 +0900

    feedback

--- yamaoKadai17/AddItemViewController.swift
+++ yamaoKadai17/AddItemViewController.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 
 class AddItemViewController: UIViewController {
@@ -6,51 +5,49 @@ class AddItemViewController: UIViewController {
     enum Mode {
         case add, edit(name: String)
     }
-    
+
     var mode = Mode.add
-    
-    
+
     @IBOutlet weak var nameTextField: UITextField!
-    
+
     var name = ""
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         switch mode {
         case .add:
             break
         case .edit(name: let name):
-            
+
             nameTextField.text = name
         }
     }
-    
-    
+
     @IBAction func pressSaveButton(sender: AnyObject) {
-                
+
         let identifier: String
-        
+
         switch mode {
         case .add:
             identifier = "exitFromAddBySaveSegue"
         case .edit:
             identifier = "exitFromEditBySaveSegue"
         }
-        
+
         performSegue(withIdentifier: identifier, sender: sender)
     }
-    
+
     @IBAction func pressCancelButton(sender: AnyObject) {
-        
+
         let identifier: String
-    
+
         switch mode {
         case .add:
             identifier = "exitFromAddByCancelSegue"
         case .edit:
             identifier = "exitFromEditByCancelSegue"
         }
-        
+
         performSegue(withIdentifier: identifier, sender: sender)
     }
 }
